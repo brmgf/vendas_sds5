670f05c667136596b735e153d926d61cec2d651b
brmgf <a16010@bcc.unifal-mg.edu.br>
2021-11-02 10:59:46 -0300

    project created

---
+++ backend/src/main/java/com/brmgf/ds_vendas/DsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.brmgf.ds_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brmgf/ds_vendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brmgf.ds_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
